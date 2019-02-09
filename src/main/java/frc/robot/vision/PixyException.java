3ec751a2e0d95dbfe17bc0b7a41268470a339fc9
Christian R <crua.839@gmail.com>
2019-02-08 20:17:45 -0500

    Updated 2/8

---
+++ src/main/java/frc/robot/vision/PixyException.java
@@ -0,0 +1,16 @@
+package frc.robot.vision;
+
+/*
+ * @Authors
+ * Christian
+ * Jackson
+ * FRC 839 
+ */
+
+	public class PixyException extends Exception{
+		public PixyException(String message){
+			super(message);
+		}
+}
+
+
